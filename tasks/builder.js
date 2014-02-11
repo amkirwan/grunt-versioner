347b9bf6ebb4570fbb181d7212d3bf7c64c87a3a
Anthony Kirwan <amkirwan@gmail.com>
2014-02-11 13:35:37 -0600

    moved functions inside of multitask

--- tasks/builder.js
+++ tasks/builder.js
@@ -13,18 +13,7 @@ module.exports = function(grunt) {
   var shell = require('shelljs');
   var semver = require('semver');
 
-  var newVersion; 
-  var versionFileRegExp = /^([\d||A-a|.|-]+)$/im;
-  var versionJSFileRegExp = /([\'|\"]?version[\'|\"]?\s*[:|=]\s*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
-  var readmeRegExp;  
-  var versionType;
-  var tagName;
-  var tagMessage;
-  var commitMessage;
-
   grunt.registerMultiTask('builder', 'Grunt plugin for versioning, building and tagging your Git project.', function(versionType) {
-    // reset newVersion for each grunt task
-    newVersion = undefined;
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
@@ -35,8 +24,11 @@ module.exports = function(grunt) {
       tagMessage: 'Version <%= newVersion %>',
       commitMesage: 'Version <%= newVersion %>'
     });
-    // create readmeRegExp with option
-    readmeRegExp = new RegExp("(^" + options.readmeText + ".*\\[)([\\d|.|\\-|a-z]+)(\\].*\\/)([\\d|.|\\-|a-z]+)(\\).*)", "img");
+
+    var newVersion;
+    var versionFileRegExp = /^([\d||A-a|.|-]+)$/im;
+    var versionJSFileRegExp = /([\'|\"]?version[\'|\"]?\s*[:|=]\s*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
+    var readmeRegExp = new RegExp("(^" + options.readmeText + ".*\\[)([\\d|.|\\-|a-z]+)(\\].*\\/)([\\d|.|\\-|a-z]+)(\\).*)", "img");
 
     // setup the newVersion
     (function() {
@@ -49,7 +41,7 @@ module.exports = function(grunt) {
       // get the current version
       // var version = grunt.file.match(options.matchBase, 'package.json') ? grunt.file.readJSON(options.file).version : grunt.file.read(options.file);
       var version = (grunt.file.isMatch({matchBase: true}, '*.json', options.file)) ? grunt.file.readJSON(options.file).version : grunt.file.read(options.file);
-      bumpIt(version, options); // set the newVersion
+      bumpIt(version); // set the newVersion
     })();
 
     var templateData = {
@@ -79,7 +71,7 @@ module.exports = function(grunt) {
         return grunt.file.read(filepath);
       }).toString();
     
-      var updatedContent = bumpVersion(content, options);
+      var updatedContent = bumpVersion(content);
 
       // Write the destination file.
       grunt.file.write(f.dest, updatedContent);
@@ -88,40 +80,36 @@ module.exports = function(grunt) {
       grunt.log.writeln('File "' + f.dest + '" created.');
     });
 
-
     function gitAdd(file) {
       shell.exec('git add ' + file.src);
     }
 
+    function bumpIt(parsedVersion) {
+      if (options.setVersion !== undefined && newVersion === undefined) {
+        newVersion = options.setVersion;
+      } else if (newVersion === undefined) {
+        newVersion = semver.inc(parsedVersion, options.versionType || 'patch');
+      }
+      return newVersion;
+    }
+
+    function bumpVersion(content) {
+      var newContent;
+      if (content.match(readmeRegExp)) {
+        newContent = content.replace(readmeRegExp, function(match, leadText, parsedVersion, urlUpToTag, versionUrl, endText,  offset, string) {
+          return  leadText + newVersion + urlUpToTag + options.tagName + endText;
+        });    
+      } else if (content.match(versionJSFileRegExp)) {
+        newContent = content.replace(versionJSFileRegExp, function(match, prefix, parsedVersion, suffix) {
+          return prefix + newVersion + suffix;
+        });
+      } else if (content.match(versionFileRegExp)) {
+        newContent = content.replace(versionFileRegExp, function(match, parsedVersion) {
+          return newVersion;
+        });
+      }
+      return newContent;
+    } 
+
   });
-
-  
-
-  var bumpIt = function(parsedVersion, opts) {
-    if (opts.setVersion !== undefined && newVersion === undefined) {
-      newVersion = opts.setVersion;
-    } else if (newVersion === undefined) {
-      newVersion = semver.inc(parsedVersion, opts.versionType || 'patch');
-    }
-    return newVersion;
-  };
-
-  var bumpVersion = function(content, opts) {
-    var newContent;
-    if (content.match(readmeRegExp)) {
-      newContent = content.replace(readmeRegExp, function(match, leadText, parsedVersion, urlUpToTag, versionUrl, endText,  offset, string) {
-        return  leadText + newVersion + urlUpToTag + opts.tagName + endText;
-      });    
-    } else if (content.match(versionJSFileRegExp)) {
-      newContent = content.replace(versionJSFileRegExp, function(match, prefix, parsedVersion, suffix) {
-        return prefix + newVersion + suffix;
-      });
-    } else if (content.match(versionFileRegExp)) {
-      newContent = content.replace(versionFileRegExp, function(match, parsedVersion) {
-        return newVersion;
-      });
-    }
-    return newContent;
-  }; 
-
 };
